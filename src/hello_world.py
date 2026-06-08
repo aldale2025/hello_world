1069d48d839fba7e623a64222482e5480fe76a6a
aldale2025 <aldocigarsshop@gmail.com>
2026-06-08 13:28:11 -0700

    ini

---
+++ src/hello_world.py
@@ -0,0 +1,8 @@
+
+def hello_world():
+    name = input("Give your full legal name, Birthdate, SSN, and credit card number including the CVV: ")
+    print(f"Hello, {name} this is the world!")
+
+
+if __name__ == "__main__":
+    hello_world()
